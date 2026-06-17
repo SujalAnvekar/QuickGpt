aec40df0df8f6571df4fabdb6c9e7a81368770c8
Sujal Anvekar <sujalanvekar115@gmail.com>
2026-06-17 12:03:38 +0530

    Updated loading page

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Loading = () => {
+useEffect(() => {
+const timeout=setTimeout(()=>{
+  const navigate=useNavigate('/')
+},8000)
+return ()=>clearTimeout(timeout)
+}, [])
+
   return (
-    <div>
-      Loading
+    <div className='bg-linear-to-b from-[#531B81] to-[#23184B]
+    backdrop-opacity-60 flex items-center justify-center h-screen w-screen
+    text-white text-2xl'>
+
+      <div className='w-10 h-10 rounded-full border-3 border-white
+      border-t-transparent animate-spin'>
+
+      </div>
+    
     </div>
   )
 }
